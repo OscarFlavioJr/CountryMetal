e2fd9c4b2ce08a8c3ebbc96424cdf76df269eea8
Oscar Flávio Jr <oscarflaviojunior@gmail.com>
2025-11-22 20:12:59 -0300

    Fix of removind accidental submodule

---
+++ backend/app/controllers/sessions_controller.rb
@@ -0,0 +1,10 @@
+class SessionsController < ApplicationController
+    def create 
+        @user = User.find_by(name: params[:name])
+
+        if !!@user && user.authenticate(params[:password])
+            session[:user_id] = @user.id
+        else
+        end
+    end
+end
